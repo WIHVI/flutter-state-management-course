51e0d6dcd32e6fc0482516a3404b1d047a1e6c3d
Mihai Cojocaru <mihaicojocaru@Mihais-MacBook-Air.local>
2024-07-16 11:21:32 +0300

    Bloc (Part 4 of 5)

---
+++ Bloc/testingbloc_course4/lib/bloc/bottom_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:testingbloc_course4/bloc/app_bloc.dart';
+
+class BottomBloc extends AppBloc {
+  BottomBloc({
+    super.waitBeforeLoading,
+    required super.urls,
+  });
+}
---
+++ Bloc/testingbloc_course4/lib/bloc/top_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:testingbloc_course4/bloc/app_bloc.dart';
+
+class TopBloc extends AppBloc {
+  TopBloc({
+    super.waitBeforeLoading,
+    required super.urls,
+  });
+}
